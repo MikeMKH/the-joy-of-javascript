1e94146be279cc8b9b1164f1ef5567603c344768
Mike Harris <MikeMKH@users.noreply.github.com>
2021-08-12 06:33:37 -0500

    add 2.1

--- ch02/test/test.js
+++ ch02/test/test.js
@@ -1,8 +1,28 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+describe('2.1', function() {
+  describe('Object.create', function() {
+    it('should crete object from proto', function() {
+      var proto = {
+        sender: 'mharris@sender.com'
+      };
+      const child = Object.create(proto);
+      child.recipient = 'kharris@recipient.com';
+      
+      assert.equal(child.sender, 'mharris@sender.com');
+      assert.equal(child.recipient, 'kharris@recipient.com');
+    }),
+    it('should create object with links', function() {
+      const transaction = {
+        sender: 'mharris@sender.com',
+        recipient: 'kharris@recipient.com'
+      };
+      const moneyTransaction = Object.create(transaction);
+      moneyTransaction.funds = 0.0;
+      moneyTransaction.addFunds = function(amount = 0.0) { this.funds += amount; };
+      
+      moneyTransaction.addFunds(100.0);
+      assert.equal(moneyTransaction.funds, 100.0);
+      assert.ok(Object.getPrototypeOf(moneyTransaction) === transaction);
     });
   });
 });
