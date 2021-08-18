0c6df3b88b0211228658c42f54df4f6874163ed0
Mike Harris <MikeMKH@users.noreply.github.com>
2021-08-18 06:16:05 -0500

    add 3.1

--- ch03/test/test.js
+++ ch03/test/test.js
@@ -1,9 +1,29 @@
 var assert = require('assert');
 
 describe('3.1', function() {
-  it('should print "Something, Something, Something, Dark Side"', function() {
-    var s = 'Something, Something, Something, Dark Side';
-    function foo() { return s; }
-    assert.equal(foo(), s);
+  const UpperCaseFormatter = {
+    format: function(str) {
+      return str.toUpperCase();
+    }
+  };
+  it('implicit link, is-a', function() {
+    // Foo is-a UpperCaseFormatter
+    const Foo = Object.create(UpperCaseFormatter);
+    Foo.say = function say(msg) {
+      return this.format(msg);
+    };
+    assert.equal(Foo.say("hello"), "HELLO");
+  }),
+  it('explicit link, has-a', function() {
+    // Foo has-a UpperCaseFormatter
+    const Foo = {
+      formatter: UpperCaseFormatter,
+      say: function say(msg) {
+        return this.formatter !== null
+        ? this.formatter.format(msg)
+        : msg;
+      }
+    };
+    assert.equal(Foo.say("hello"), "HELLO");
   });
 });
