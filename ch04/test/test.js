48477429e1f1352322912ea5eade7ae571a0d9fc
Mike Harris <MikeMKH@users.noreply.github.com>
2021-08-30 06:56:25 -0500

    add 4.3

--- ch04/test/test.js
+++ ch04/test/test.js
@@ -1,4 +1,6 @@
 var assert = require('assert');
+const compose2 = (f, g) => (...args) => f(g(...args));
+const compose = (...fns) => fns.reduce(compose2);
 
 describe('4.0', function() {
   it('functions are objects', function() {
@@ -53,6 +55,67 @@ describe('4.2', function() {
         .map(x => x * 3)
         .reduce((m, x) => m + x);
       assert.equal(sum, 60);
+    });
+  });
+});
+
+describe('4.3', function() {
+  describe('compose2', function() {
+    it('should be able to compose functions', function() {
+      const add1 = x => x + 1;
+      const multiply2 = x => x * 2;
+      const add1AndMultiply2 = compose2(add1, multiply2);
+      assert.equal(add1AndMultiply2(3), (3 * 2) + 1);
+    }),
+    it('should be able to compose functions which use strings', function() {
+      const count = arr => arr.length;
+      const split = str => str.split(/\s+/);
+      const countWords = compose2(count, split);
+      assert.equal(countWords('hello world'), 2);
+      assert.equal(countWords('the quick brown fox jump over the box'), 8);
+    });
+  }),
+  describe('compose', function() {
+    it('should be able to compose functions', function() {
+      const add1 = x => x + 1;
+      const multiply2 = x => x * 2;
+      const multiply4 = compose2(multiply2, multiply2);
+      const f = compose(multiply4, add1, multiply2);
+      assert.equal(f(3), ((3 * 2) + 1) * 4);
+    });
+  });
+  describe('HasHash', function() {
+    const computeCipher = (data, i = 0, hash = 0) => {
+      if(i >= data.length) {
+        return hash ** 2;
+      }
+      return computeCipher(
+        data,
+        i + 1,
+        ((hash << 5) - hash + data.charCodeAt(i)) << 0
+      );
+    };
+    const assemble = keys => obj => keys.map(key => obj[key]).join(' ');
+    
+    describe('computeCipher', function() {
+      it('should be able to compute a cipher', function() {
+        assert.equal(computeCipher('hello world'), 3218816525823026700);
+        assert.equal(computeCipher('John Doe'), 1869562306066055700);
+        assert.equal(computeCipher('John Doe'), 1869562306066055700);
+        assert.equal(computeCipher('the quick brown fox jump over the box'), 258019758144341220);
+      });
+    }),
+    describe('assemble', function() {
+      it('should be able to grab given key values from an object', function() {
+        const getName = assemble(['first', 'last']);
+        assert.deepEqual(getName({first: 'John', last: 'Doe', other: 42}), 'John Doe');
+      });
+    }),
+    describe('computeHash', function() {
+      it('should be able to compute a hash', function() {
+        const computeHash = compose(computeCipher, assemble(['first', 'last']));
+        assert.equal(computeHash({first: 'John', last: 'Doe', other: 'stuff'}), 1869562306066055700);
+      });
     });
   });
 });
