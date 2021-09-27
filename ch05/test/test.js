8d937f11fd75a5505a0c278fdb83e8a2916c2469
Mike Harris <MikeMKH@users.noreply.github.com>
2021-09-27 05:57:25 -0500

    refactor

--- ch05/test/test.js
+++ ch05/test/test.js
@@ -1,20 +1,20 @@
 const { equal } = require('assert');
 var assert = require('assert');
-const { get } = require('http');
 const compose2 = (f, g) => (...args) => f(g(...args));
 const compose = (...fns) => fns.reduce(compose2);
 const composeM2 = (f, g) => (...args) => g(...args).flatMap(f);
 const composeM = (...Ms) => Ms.reduce(composeM2);
 const pipe = (...fns) => fns.reduceRight(compose2);
 const curry = fn => (...args1) =>
-args1.length === fn.length
-? fn(...args1)
-: (...args2) => {
-  const args = [...args1, ...args2];
-  return args.length >= fn.length
-  ? fn(...args)
-  : curry(fn)(...args);
+  args1.length === fn.length
+  ? fn(...args1)
+  : (...args2) => {
+    const args = [...args1, ...args2];
+    return args.length >= fn.length
+      ? fn(...args)
+      : curry(fn)(...args);
 };
+
 const filter = curry((f, a) => a.filter(f));
 const map = curry((f, a) => a.map(f));
 const reduce = curry((f, a) => a.reduce(f));
