1b24649193298babe35bf29588c1de91cbe62377
Mike Harris <MikeMKH@users.noreply.github.com>
2021-09-27 06:18:26 -0500

    add 6.2.1

--- ch06/test/test.js
+++ ch06/test/test.js
@@ -15,8 +15,31 @@ const curry = fn => (...args1) =>
       : curry(fn)(...args);
 };
 
-describe('test', function() {
-  it('should pass', function() {
-    assert.ok(true);
+describe('define class in an object namespace', function() {
+  let MyObjectClass = global.MyObjectClass || {};
+  MyObjectClass.domain = {};
+  
+  MyObjectClass.domain.MyObject = class {
+    #feePercent = 0.6;
+    constructor(sender, receiver, funds = 0.0) {
+      this.sender = sender;
+      this.receiver = receiver;
+      this.funds = Number(funds);
+      this.timestamp = Date.now();
+    }
+    
+    static #precisionRounding(number, precision) {
+      const factor = Math.pow(10, precision);
+      return Math.round(number * factor) / factor;
+    }
+    
+    netTotal() {
+      return MyObjectClass.domain.MyObject.#precisionRounding(this.funds * this.#feePercent, 2);
+    }
+  }
+  
+  it('should calculate netTotal', function() {
+    let myObject = new MyObjectClass.domain.MyObject('sender', 'receiver', 100.0);
+    assert.equal(myObject.netTotal(), 60.0);    
   })
-})
+});
