0c2553382503663fb7aa71758b10d39162fc7d77
Mike Harris <MikeMKH@users.noreply.github.com>
2021-09-27 06:01:04 -0500

    init

---
+++ ch06/test/test.js
@@ -0,0 +1,22 @@
+const { equal } = require('assert');
+var assert = require('assert');
+const compose2 = (f, g) => (...args) => f(g(...args));
+const compose = (...fns) => fns.reduce(compose2);
+const composeM2 = (f, g) => (...args) => g(...args).flatMap(f);
+const composeM = (...Ms) => Ms.reduce(composeM2);
+const pipe = (...fns) => fns.reduceRight(compose2);
+const curry = fn => (...args1) =>
+  args1.length === fn.length
+  ? fn(...args1)
+  : (...args2) => {
+    const args = [...args1, ...args2];
+    return args.length >= fn.length
+      ? fn(...args)
+      : curry(fn)(...args);
+};
+
+describe('test', function() {
+  it('should pass', function() {
+    assert.ok(true);
+  })
+})
