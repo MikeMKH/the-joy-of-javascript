656b1159d555b55492607679f4f00bde7f306962
Mike Harris <MikeMKH@users.noreply.github.com>
2021-10-04 06:12:42 -0500

    remove unused

--- ch06/test/test.js
+++ ch06/test/test.js
@@ -2,21 +2,6 @@
 const { equal } = require('assert');
 const assert = require('assert');
 
-const compose2 = (f, g) => (...args) => f(g(...args));
-const compose = (...fns) => fns.reduce(compose2);
-const composeM2 = (f, g) => (...args) => g(...args).flatMap(f);
-const composeM = (...Ms) => Ms.reduce(composeM2);
-const pipe = (...fns) => fns.reduceRight(compose2);
-const curry = fn => (...args1) =>
-  args1.length === fn.length
-  ? fn(...args1)
-  : (...args2) => {
-    const args = [...args1, ...args2];
-    return args.length >= fn.length
-      ? fn(...args)
-      : curry(fn)(...args);
-};
-
 describe('class in an object namespace', function() {
   let MyObjectClass = global.MyObjectClass || {};
   MyObjectClass.domain = {};
