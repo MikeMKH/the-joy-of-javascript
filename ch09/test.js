768ace5790b191a7967bf5610bee102d07001e55
Mike Harris <MikeMKH@users.noreply.github.com>
2021-11-01 06:19:43 -0500

    add smoke test

---
+++ ch09/test.js
@@ -0,0 +1,23 @@
+const { equal } = require('assert');
+const assert = require('assert');
+
+const compose2 = (f, g) => (...args) => f(g(...args));
+const compose = (...fns) => fns.reduce(compose2);
+const composeM2 = (f, g) => (...args) => g(...args).flatMap(f);
+const composeM = (...Ms) => Ms.reduce(composeM2);
+const pipe = (...fns) => fns.reduceRight(compose2);
+const curry = fn => (...args1) =>
+  args1.length === fn.length
+  ? fn(...args1)
+  : (...args2) => {
+    const args = [...args1, ...args2];
+    return args.length >= fn.length
+      ? fn(...args)
+      : curry(fn)(...args);
+};
+
+describe('9.0', () => {
+  it('should work', () => {
+    assert.ok(compose(Math.sqrt, Math.pow)(2, 2) === 2);
+  })
+})
