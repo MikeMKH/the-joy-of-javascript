65dc05aa034ef5af9a54b0a8e62c3b49dcb06616
Mike Harris <MikeMKH@users.noreply.github.com>
2021-11-01 06:17:39 -0500

    remove unneeded

--- ch08/test/test.js
+++ ch08/test/test.js
@@ -1,21 +1,8 @@
 const { equal } = require('assert');
 const assert = require('assert');
-const { resolve } = require('path');
 
 const compose2 = (f, g) => (...args) => f(g(...args));
 const compose = (...fns) => fns.reduce(compose2);
-const composeM2 = (f, g) => (...args) => g(...args).flatMap(f);
-const composeM = (...Ms) => Ms.reduce(composeM2);
-const pipe = (...fns) => fns.reduceRight(compose2);
-const curry = fn => (...args1) =>
-  args1.length === fn.length
-  ? fn(...args1)
-  : (...args2) => {
-    const args = [...args1, ...args2];
-    return args.length >= fn.length
-      ? fn(...args)
-      : curry(fn)(...args);
-};
 
 describe('8.2', () => {
   describe('Promise', () => {
